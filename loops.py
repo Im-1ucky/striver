e70c42511ad35dbd5eeccc200e09f844903f0bed
Im-1ucky <luckymi11lite@gmail.com>
2026-06-26 19:18:21 +0530

    Five questions done

--- loops.py
+++ loops.py
@@ -3,9 +3,31 @@ def print1(n):
         print("*" * n)
 
 
+def print2(n):
+    for i in range(1, n + 1):
+        print("*" * i)
+
+
+def print3(n):
+    for i in range(1, n + 1):
+        for j in range(i):
+            print(j + 1, end="")
+        print("")
+
+
+def print4(n):
+    for i in range(1, n + 1):
+        print(f"{i}" * i)
+
+
+def print5(n):
+    for i in range(n, 0, -1):
+        print("*" * i)
+
+
 def main():
     n = int(input("Enter number: "))
-    print1(n)
+    print5(n)
 
 
 main()
